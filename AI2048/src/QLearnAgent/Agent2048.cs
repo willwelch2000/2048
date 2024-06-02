5ae8c4101d14c7042f730967feac536048875676
willwelch2000 <willwelch2000@gmail.com>
2024-06-02 16:34:29 -0500

    Updated rewards system

--- AI2048/src/QLearnAgent/Agent2048.cs
+++ AI2048/src/QLearnAgent/Agent2048.cs
@@ -94,13 +94,54 @@ public class Agent2048 : IQLearnAgent<int[,], Direction>
     }
 
     /// <summary>
-    /// Testing different scoring methods
+    /// Returns the value of tiles that were combined.
+    /// For example, if two 2s combine to form a 4, this returns 4
     /// </summary>
     /// <param name="state"></param>
     /// <param name="action"></param>
     /// <param name="nextState"></param>
     /// <returns></returns>
     public double GetReward(int[,] state, Direction action, int[,] nextState)
+    {
+        int[] stateArr = FlattenIntMatrix(state).ToArray();
+        int[] nextStateArr = FlattenIntMatrix(nextState).ToArray();
+
+        Dictionary<int, int> originalValues = [];
+        Dictionary<int, int> nextValues = [];
+        Dictionary<int, int> added = [];
+
+        // figure out which number was added as the next tile
+        int addedTile = nextStateArr.Sum() - stateArr.Sum();
+
+        // count values in original state
+        foreach(int i in stateArr)
+            if (i > 0 && !originalValues.TryAdd(i, 1))
+                originalValues[i] += 1;
+
+        // count values in next state
+        foreach(int i in nextStateArr)
+            if (i > 0 && !nextValues.TryAdd(i, 1))
+                nextValues[i] += 1;
+
+        // for every number present, figure out the difference between next state and original state
+        // i.e. the added tiles of that value
+        int nextStateMax = nextValues.Keys.Max();
+        int value = nextStateMax;
+        added[value] = 0; // initialize highest value dictionary entry
+        added[addedTile] = -1; // remove an entry from the value that was added as the additional tile--the loop will cancel out with this
+        while(value > 1)
+        {
+            added.TryAdd(value/2, 0); // initialize the next value's entry
+            added[value] += TryGetDictionaryValueOrZero(nextValues, value) - TryGetDictionaryValueOrZero(originalValues, value);
+            added[value/2] += 2 * added[value]; // do this to cancel out with the fact that there's two less of a number whenever two combine
+            value /= 2; // move to next value
+        }
+
+        // return sum of key * number of additions
+        return added.Keys.Select(k => k * added[k]).Sum();
+    }
+
+    private double OldRewardMethod(int[,] state, Direction action, int[,] nextState)
     {
         int[] flattenedState = FlattenIntMatrix(state).ToArray();
         int[] flattenedNextState = FlattenIntMatrix(nextState).ToArray();
@@ -189,57 +230,10 @@ public class Agent2048 : IQLearnAgent<int[,], Direction>
         return FlattenIntMatrix(game.Board).Max() > FlattenIntMatrix(state).Max();
     }
 
-    /// <summary>
-    /// Returns the value of squares that were combined.
-    /// For example, if two 2s combine to form a 4, this returns 4
-    /// </summary>
-    /// <param name="stateArr">state as array</param>
-    /// <param name="nextStateArr">nextState as array</param>
-    /// <returns></returns>
-    private double ValueCombined(int[] stateArr, int[] nextStateArr)
+    private static int TryGetDictionaryValueOrZero(Dictionary<int, int> dict, int key)
     {
-        Dictionary<int, int> originalValues = new();
-        Dictionary<int, int> nextValues = new();
-        Dictionary<int, int> added = new();
-
-        // count values in original state
-        foreach(int i in stateArr)
-        {
-            if (!originalValues.ContainsKey(i))
-                originalValues[i] = 0;
-            originalValues[i] += 1;
-        }
-
-        // count values in next state
-        foreach(int i in nextStateArr)
-        {
-            if (!nextValues.ContainsKey(i))
-                nextValues[i] = 0;
-            nextValues[i] += 1;
-        }
-
-        // for every number present, figure out the difference between next state and original
-        int nextStateMax = nextValues.Keys.Where(x => x > 0).Max();
-        int value = nextStateMax;
-        while(value > 1)
-        {
-            if (!added.ContainsKey(value))
-                added[value] = 0;
-            if (!added.ContainsKey(value/2))
-                added[value/2] = 0;
-            added[value] += SafeGetDictionaryValue(nextValues, value) - SafeGetDictionaryValue(originalValues, value);
-            added[value/2] += 2 * added[value];
-            value /= 2;
-        }
-
-        // return sum of key * number of occurrences
-        return added.Keys.Select(k => k * added[k]).Sum();
-    }
-
-    private int SafeGetDictionaryValue(Dictionary<int, int> dict, int key)
-    {
-        if (dict.ContainsKey(key))
-            return dict[key];
+        if (dict.TryGetValue(key, out int value))
+            return value;
         return 0;
     }
 
